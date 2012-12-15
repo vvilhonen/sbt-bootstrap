00c6f4a1278adc179f0ae3ac970c33a2067cf38c
Vesa Vilhonen <vesa@vilhonen.com>
2012-12-15 13:38:47 +0200

    Add example build.sbt

--- bootstrap_sbt.sh
+++ bootstrap_sbt.sh
@@ -4,4 +4,10 @@ chmod a+x sbt
 mkdir -p project
 mkdir -p src/main/scala
 mkdir -p src/test/scala
+if [ ! -e build.sbt ]; then
+  cat > build.sbt <<EOF
+//libraryDependencies ++= Seq(
+//  "org.scalatest" %% "scalatest" % "1.8" % "test")
+EOF
+fi
 echo 'addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.2.0")' > project/plugin.sbt
